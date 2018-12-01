b478397cbeb89e3cf45969481b1b016d70771654
Nefito <43294012+Nefito@users.noreply.github.com>
2018-12-01 13:09:09 +0200

    Update lab6(code).c

--- lab6(code).c
+++ lab6(code).c
@@ -120,43 +120,43 @@ int main()
     lexSwap(len5, a5);
     lexSwap(len6, a6);
 
-    printf("The next lexographic permutation for set 1 is:\n");
+    printf("The next lexicographic permutation for set 1 is:\n");
     for(i = 0; i < len1; i++)
     {
         printf(" %d ", a1[i]);
     }printf("\n");
 
-    printf("The next lexographic permutation for set 2 is:\n");
+    printf("The next lexicographic permutation for set 2 is:\n");
     for(i = 0; i < len2; i++)
     {
         printf(" %d ", a2[i]);
     }printf("\n");
 
-    printf("The next lexographic permutation for set 3 is:\n");
+    printf("The next lexicographic permutation for set 3 is:\n");
     for(i = 0; i < len3; i++)
     {
         printf(" %d ", a3[i]);
     }printf("\n");
 
-    printf("The next lexographic permutation for set 4 is:\n");
+    printf("The next lexicographic permutation for set 4 is:\n");
     for(i = 0; i < len4; i++)
     {
         printf(" %d ", a4[i]);
     }printf("\n");
 
-    printf("The next lexographic permutation for set 5 is:\n");
+    printf("The next lexicographic permutation for set 5 is:\n");
     for(i = 0; i < len5; i++)
     {
         printf(" %d ", a5[i]);
     }printf("\n");
 
-    printf("The next lexographic permutation for set 6 is:\n");
+    printf("The next lexicographic permutation for set 6 is:\n");
     for(i = 0; i < len6; i++)
     {
         printf(" %d ", a6[i]);
     }printf("\n");
 
-    //this is for the binominal theorem
+    //this is for the binomial theorem
     printf("Enter a value for x: \n");
     scanf("%d", &x);
 
